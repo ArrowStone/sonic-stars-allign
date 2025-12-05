cb7a7e89cb096e9248b771154530fec4d15abe98
Ice Baron <garill@outlook.com>
2025-12-05 14:02:13 +0000

    Light Dash adjustments

--- SonicStarsAlign/Assets/SSA-Framework/Objects/Automation/ObjectAlongSpline.cs
+++ SonicStarsAlign/Assets/SSA-Framework/Objects/Automation/ObjectAlongSpline.cs
@@ -5,6 +5,7 @@ public class ObjectAlongSpline : MonoBehaviour
 {
     public GameObject Object;
     public float Count;
+    public bool rotateObjects = true;
     public SplineContainer Spline;
 
     [ContextMenu("Regenerate")]
@@ -15,10 +16,22 @@ public class ObjectAlongSpline : MonoBehaviour
             DestroyImmediate(transform.GetChild(i).gameObject);
         }
 
+        GameObject currentObject = null; // For rotation
+        GameObject prevObject;
+
         for (float i = 0; i < 1; i += 1 / Count)
         {
+            prevObject = currentObject;
+
             Vector3 pos = Spline.EvaluatePosition(i);
-            _ = Instantiate(Object, pos, Quaternion.identity, transform);
+            currentObject = Instantiate(Object, pos, Quaternion.identity, transform);
+
+            if (prevObject & rotateObjects)
+            {
+                Vector3 lookPos = prevObject.transform.position;
+                lookPos.y = currentObject.transform.position.y;
+                currentObject.transform.LookAt(lookPos);
+            }
         }
     }
 }
