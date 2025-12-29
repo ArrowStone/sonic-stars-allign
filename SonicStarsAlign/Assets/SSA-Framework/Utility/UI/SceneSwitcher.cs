e7c325e553a41664f0764945a4664105407b5f5d
Ice Baron <garill@outlook.com>
2025-12-29 16:25:56 +0000

    Menu visual improvements

--- SonicStarsAlign/Assets/SSA-Framework/Utility/UI/SceneSwitcher.cs
+++ SonicStarsAlign/Assets/SSA-Framework/Utility/UI/SceneSwitcher.cs
@@ -3,8 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    private int cachedScene = 0;
+
     public void SwitchScene(int _scn)
     {
-        SceneManager.LoadSceneAsync(_scn);
+        if (_scn < 0)
+        {
+            ExitGame();
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(_scn);
+        }
+    }
+    public void AddScene(int _scn)
+    {
+        SceneManager.LoadSceneAsync(_scn, LoadSceneMode.Additive);
+    }
+    private void ExitGame()
+    {
+        // Credit: Unity Docs
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void CacheScene(int _scn)
+    {
+        cachedScene = _scn;
+    }
+    public void LoadCachedScene()
+    {
+        SwitchScene(cachedScene);
     }
 }
