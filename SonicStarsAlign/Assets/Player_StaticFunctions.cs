9cfda8ba241eb62ab92fbb3c83237dbd2bd3c112
Django <charlesdc12446@gmail.com>
2026-02-13 16:49:23 +0000

    Testing character interpolation and cleaning debug logs.

--- SonicStarsAlign/Assets/Player_StaticFunctions.cs
+++ SonicStarsAlign/Assets/Player_StaticFunctions.cs
@@ -18,23 +18,24 @@ public class Player_StaticFunctions : MonoBehaviour
 
         //Called every time the player's transform is set manually, to allow for debugging code everytime it happens.
         public static void SetTransform (Transform transform, Vector3 position, Quaternion rotation, string source) {
-                Debug.Log(_fixedFrameNumber+ " " +source+ " has set Player Transform to " + position + " and " + rotation + "!");
+                //Debug.Log(_fixedFrameNumber+ " " +source+ " has set Player Transform to " + position + " and " + rotation + "!");
                 Debug.DrawLine(transform.position, position, Color.red, 10f);
 
                 transform.SetPositionAndRotation(position, rotation);
         }
 
         public static void MoveRBPosition(Rigidbody rb, Vector3 position, string source) {
-                Debug.Log(_fixedFrameNumber + " " + source + " has moved Player RB Position to " + position + "!");
+                //Debug.Log(_fixedFrameNumber + " " + source + " has moved Player RB Position to " + position + "!");
+                //Debug.Log(source + " has moved Player RB Position to  !");
 
-                Debug.DrawRay(rb.transform.position, Vector3.up, Color.blue, 10f);
-                Debug.DrawRay(position, Vector3.up, Color.blue, 10f);
-                Debug.DrawLine(rb.transform.position, position, Color.blue, 10f);
+                //Debug.DrawRay(rb.transform.position, Vector3.up, Color.blue, 10f);
+                //Debug.DrawRay(position, Vector3.up, Color.blue, 10f);
+                //Debug.DrawLine(rb.transform.position, position, Color.blue, 10f);
                 rb.MovePosition(position);
         }
 
         public static void SetRBPosition( Rigidbody rb, Vector3 position, string source ) {
-                Debug.Log(_fixedFrameNumber + " " + source + " has set Player RB Position to " + position + "!");
+                //Debug.Log(_fixedFrameNumber + " " + source + " has set Player RB Position to " + position + "!");
 
                 Debug.DrawLine(rb.position, position, Color.magenta, 10f);
                 rb.transform.position = position;
@@ -42,7 +43,7 @@ public class Player_StaticFunctions : MonoBehaviour
         
 
         public static void SetTransformRotation(Transform transform, Quaternion rotation, string source) {
-                Debug.Log(_fixedFrameNumber + " " + source + " has set Player Transform Rotation to " + rotation + "!");
+                //Debug.Log(_fixedFrameNumber + " " + source + " has set Player Transform Rotation to " + rotation + "!");
                 Debug.DrawRay(transform.position, transform.rotation * Vector3.up, Color.green, 10f);
                 Debug.DrawRay(transform.position, rotation * Vector3.up, Color.green, 10f);
 
