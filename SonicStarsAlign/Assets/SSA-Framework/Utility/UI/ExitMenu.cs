dbfd39021894412c7b9cd59d1aa90362cac9ee64
Ice Baron <garill@outlook.com>
2026-04-12 18:52:33 +0100

    Menu fixes

---
+++ SonicStarsAlign/Assets/SSA-Framework/Utility/UI/ExitMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// Used to exit the menus in the main menu which are separate scenes
+public class ExitMenu : MonoBehaviour
+{
+    public void Exit()
+    {
+        foreach(Canvas canvas in FindObjectsByType<Canvas>(FindObjectsSortMode.None))
+        {
+            if(canvas.gameObject.scene.buildIndex != gameObject.scene.buildIndex)
+            {
+                canvas.GetComponent<Animator>().SetInteger("Menu", 0);
+                return;
+            }   
+        }
+    }
+}
